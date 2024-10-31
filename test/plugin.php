f10f8c6606ad0d5ac168e7b7d5b610ae69fe9b89
Matteo Greco <matteo@matteogreco.net>
2024-10-31 22:59:07 +0100

    test: change testing strategy

---
+++ test/plugin.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Test Plugin
+ * Plugin URI: https://example.com/test-plugin
+ * Description: A test plugin for version bumping
+ * Version: 1.2.3
+ * Author: Test Author
+ * Author URI: https://example.com
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: test-plugin
+ * Domain Path: /languages
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) exit;
