26f57521d9646df163e17992503202d035551420
Matteo Greco <matteo@matteogreco.net>
2024-11-22 07:01:42 +0100

    chore: add configuration files for Prettier, ESLint, and initial package setup

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: ['eslint:recommended'],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-duplicate-imports': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'eol-last': ['error', 'always'],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/valid-expect': 'error',
+  },
+};
